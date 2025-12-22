b2010eed67ee8266269039fd525248bf31b3d856
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-23 05:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
-const inputs = document.querySelectorAll('.controls input');
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const toggle = player.querySelector('.player__button');
+const progress = player.querySelector('.progress');
+const progressFilled = player.querySelector('.progress__filled');
+const volume = player.querySelector('.volume');
+const playbackSpeed = player.querySelector('.playbackSpeed');
+const rewind = player.querySelector('.rewind');
+const forward = player.querySelector('.forward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Toggle play / pause
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Update play button icon
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+// Update progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = `${percent}%`;
+}
+
+// Scrub video
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+// Volume & speed
+volume.addEventListener('input', () => {
+  video.volume = volume.value;
+});
+
+playbackSpeed.addEventListener('input', () => {
+  video.playbackRate = playbackSpeed.value;
+});
+
+// Skip buttons
+rewind.addEventListener('click', () => {
+  video.currentTime -= 10;
+});
+
+forward.addEventListener('click', () => {
+  video.currentTime += 25;
+});
+
+// Progress bar click
+progress.addEventListener('click', scrub);
